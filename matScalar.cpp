8e44d0aa53f94a1511119a94bba9145f19bed5ff
surenravi1407 <surenravi1407@gmail.com>
2023-07-08 17:00:33 +0300

    Element-Wise Mul Ops: Mat * Scalar

--- matScalar.cpp
+++ matScalar.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 // Element-wise ops
 
-void matScalar(int rowSize, int columnSize, int scalar)
+void matScalar(int rowSize, int columnSize, int scalar, int values)
 {
-    int matrix[rowSize][columnSize] = {{3, 3}, {4, 4}, {5, 5}, {6, 6}, {7, 7}};
+    int matrix[rowSize][columnSize] = {{3, 3}, {4, 4}, {5, 5}, {6, 6}, {7, 7}}; // Replace values with arrValues
     for (int rowIndex = 0; rowIndex < rowSize; rowIndex++)
     {
         for (int columnIndex = 0; columnIndex < columnSize; columnIndex++)
@@ -19,12 +20,14 @@ void matScalar(int rowSize, int columnSize, int scalar)
 
 int main()
 {
-    int rowSize, columnSize, scalar;
+    int rowSize, columnSize, scalar, values;
+    cout << "Enter Matrix Values: " << endl;
+    cin >> values;
     cout << "Enter Row Dimension = ";
     cin >> rowSize;
     cout << "Enter Column Dimension = ";
     cin >> columnSize;
     cout << "Enter Scalar Value = ";
     cin >> scalar;
-    matScalar(rowSize, columnSize, scalar);
+    matScalar(rowSize, columnSize, scalar, values);
 }
